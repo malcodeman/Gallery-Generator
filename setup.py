74ddb7f1591259499e2d919446c789ec4611ff41
malcodeman <malcodeman@users.noreply.github.com>
2017-07-24 17:57:29 +0200

    setup.py update

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 from setuptools import setup, find_packages
 
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
 setup(name='vestibulum',
       version='0.1',
       description='Static gallery generator',
-      url='https://github.com/malcodeman/Gallery-Generator',
+      long_description=readme,
+      url='https://github.com/malcodeman/Vestibulum',
       author='malcodeman',
       license=license,
       py_modules=['vestibulum/main'],
       packages=find_packages(exclude=('tests', 'docs')),
       entry_points = {
-        'console_scripts': ['vesti build = vestibulum.main:main'],
+        'console_scripts': ['vestibulum = vestibulum.main:main'],
       },
       zip_safe=False)
