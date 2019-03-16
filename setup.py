8e8a72ebbcbee1cf772945acb60d442f5a4c9541
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2019-03-16 17:06:10 +0100

    Improves packaging

--- setup.py
+++ setup.py
@@ -1,21 +1,19 @@
-from setuptools import setup, find_packages
+import setuptools
 
-with open('README.md') as f:
-    readme = f.read()
 
-with open('LICENSE') as f:
-    license = f.read()
+def readme():
+    with open('README.md') as f:
+        return f.read()
 
-setup(name='pygram',
-      version='0.1',
-      description='Static gallery generator',
-      long_description=readme,
-      url='https://github.com/malcodeman/Pygram',
-      author='malcodeman',
-      license=license,
-      py_modules=['pygram/main'],
-      packages=find_packages(exclude=('tests', 'docs')),
-      entry_points = {
-        'console_scripts': ['pygram = pygram.main:main'],
-      },
-      zip_safe=False)
+
+setuptools.setup(name='pygram',
+                 version='0.1',
+                 description='Static gallery generator',
+                 long_description=readme(),
+                 url='https://github.com/malcodeman/pygram',
+                 author='malcodeman',
+                 packages=setuptools.find_packages(),
+                 entry_points={
+                     'console_scripts': ['pygram = pygram.main:main'],
+                 },
+                 zip_safe=False)
